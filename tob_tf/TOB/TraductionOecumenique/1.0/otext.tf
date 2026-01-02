d17e84e62d0fe95cfe5bf82b057752372e160b5a
Ronan Guilloux <ronan.guilloux@gmail.com>
2026-01-02 14:39:57 +0100

    removing tob files

--- tob_tf/TOB/TraductionOecumenique/1.0/otext.tf
+++
@@ -1,9 +0,0 @@
-@config
-@fmt:text-orig-full={text}
-@sectionFeatures=book,chapter,verse
-@sectionTypes=book,chapter,verse
-@structureFeatures=book,chapter,verse
-@structureTypes=book,chapter,verse
-@writtenBy=Text-Fabric
-@dateWritten=2025-11-22T12:05:38+00:00Z
-
--- tob_tf/TOB/TraductionOecumenique/1.0/otype.tf
+++
@@ -1,8 +0,0 @@
-@node
-@valueType=str
-@writtenBy=Text-Fabric
-@dateWritten=2025-11-22T12:05:38+00:00Z
-
-1-31094	verse
-31095-32280	chapter
-32281-32345	book
